89eb33f85fd8fe61d97f34b3c263dce83d1ade1a
Alessandro <acordellam@hotmail.com>
2025-09-24 20:01:06 -0300

    acordellam
    informatica e mecanica
    
    Signed-off-by: Alessandro <acordellam@hotmail.com>

---
+++ GlobalUsings.cs
@@ -0,0 +1,6 @@
+global using acordellam.Data;
+global using acordellam.PageModels;
+global using acordellam.Pages;
+global using acordellam.Services;
+global using acordellam.Utilities;
+global using Fonts;
